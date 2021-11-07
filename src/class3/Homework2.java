d094edc88a1cdd8119eb7c08ddb5e922e9c02ac2
Sabina Yasmin <sabinayasmin@sabinas-air.home>
2021-11-07 10:39:26 -0500

    Class3

--- src/class3/Homework2.java
+++ src/class3/Homework2.java
@@ -1,5 +1,7 @@
 package class3;
 
+import java.time.Year;
+
 public class Homework2 {
     /**
      * Store values for below scenario in an appropriate variable, and print the same console
@@ -13,4 +15,35 @@ public class Homework2 {
      * Initials of your name (Use: char)
      *
      */
+    public static void main (String [] args){
+        short numberofHoursInDay = 24;
+        short numberofDaysInYear = 365;
+        int toTalNumberofEmployee = 1056;
+        int PopulationInBangladesh = 166880617;
+        double mortgageRate = 3.275;
+        int BalanceInBank = 1508;
+        char myFirstName = 'S';
+        char LastName = 'Y';
+        boolean SunriseIntheWest = true;
+
+
+
+
+
+        System.out.println("Number of Hours In a Day =" + numberofHoursInDay);
+        System.out.println("Number of Days In a Year =" + numberofDaysInYear);
+        System.out.println("Total number of Employees in an Organization =" + toTalNumberofEmployee);
+        System.out.println("Population of Bangladesh =" + PopulationInBangladesh);
+        System.out.println("Mortgage Rate =" + mortgageRate);
+        System.out.println("Balance in a Bank Account =" + BalanceInBank);
+        System.out.println("Initial of my name =" + myFirstName + LastName );
+        System.out.println("Does Sun Rises In the West? =" + SunriseIntheWest);
+
+
+
+
+
+
+
+    }
 }
